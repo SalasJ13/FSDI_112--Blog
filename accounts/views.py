9e83879a206e349ac585ca094292cc79a1918d1a
SalasJ13 <salasjazmin15@gmail.com>
2022-04-02 17:27:22 -0700

    Heroku app

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
+from django.views.generic.edit import CreateView, FormView
 from django.urls import reverse_lazy
 
 
@@ -7,3 +7,9 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class PasswordChangeView(FormView):
+    form_class = PasswordResetForm
+    success_url = reverse_lazy('password_change_done')
+    template_name = 'registration/password_change_form.html'
